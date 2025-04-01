2dda73409488dcea83e8362ef2cc270cc73ddbea
Cuong Nguyen <ntcuong2103@gmail.com>
2025-04-01 15:31:59 +0000

    remove wandb

--- train.py
+++ train.py
@@ -1,18 +1,11 @@
 from ultralytics import YOLO
 project = "nom-detection"
 name = "yolo10m-1280p"
-import wandb
-wandb.init(project=project, name=name)
-from wandb.integration.ultralytics import add_wandb_callback
-
-
 
 # Load a model
 # model = YOLO("yolov10m.pt")  # load a pretrained model (recommended for training)
 model = YOLO("nom-detection/yolo10m-12803/weights/best.pt")  # load a pretrained model (recommended for training)
 # model = YOLO("yolo11n.yaml").load("yolo11n.pt")  # build from YAML and transfer weights
-
-add_wandb_callback(model, enable_model_checkpointing=True)
 
 # Train the model
 model.train(data="datasets/tkh-mth2k2/tkh-mth2k2.yaml",
@@ -31,4 +24,3 @@ model.train(data="datasets/tkh-mth2k2/tkh-mth2k2.yaml",
             plots=False,
             crop_fraction=0.5,
             pretrained=True)
-wandb.finish()
