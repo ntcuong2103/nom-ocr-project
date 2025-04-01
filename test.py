c747890c9f141b40db85f64ef69784ca45ef8ced
Cuong Tuan Nguyen <ntcuong2103@gmail.com>
2025-04-01 09:01:07 +0000

    update test

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from ultralytics import YOLO
-
+import os
+import glob
 project = "nom-detection"
 
 # model = YOLO("models/yolo10m-1280-tkh.pt")
@@ -12,7 +13,9 @@ name = "yolo10m-1280-tkh-mth-test"
 # name = "yolo11n-1280-tkh-test"
 
 
-def generate_gt(model, path="datasets/nomnaocr/val"):
+def generate_gt(model_checkpoint, path, output_folder):
+
+    model = YOLO(model_checkpoint)
     model.predict(
         path,
         max_det=500,
@@ -21,8 +24,8 @@ def generate_gt(model, path="datasets/nomnaocr/val"):
         save_txt=True,
         save_conf=True,
         batch=4,
-        project=project,
-        name=name,
+        project=output_folder,
+        name=os.path.splitext(os.path.basename(model_checkpoint))[0],
         imgsz=1280,
         single_cls=True,
         verbose=True,
@@ -51,5 +54,14 @@ def generate_pred(model, path="datasets/nomnaocr/val"):
     )
 
 
-# generate_pred(model)
-generate_gt(model)
+if __name__ == "__main__":
+    # parse args
+    import argparse
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", type=str, default="models/yolo10m-1280-tkh-mth.pt")
+    parser.add_argument("--path", type=str, default="datasets/nomnaocr/Pages/**/*.jpg")
+    parser.add_argument("--output", type=str, default="nom-detection-gt")
+    args = parser.parse_args()
+
+    generate_gt(args.model, args.path, args.output)
