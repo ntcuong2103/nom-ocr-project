c816501ac0f42e9d63be8ca6662ca6678442aa1c
Cuong Tuan Nguyen <ntcuong2103@gmail.com>
2026-02-15 19:03:19 +0000

    update description

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
-# generate detection results
+# Step 1: Copy the dataset to folder "nomnaocr-val" using nomnaocr-val-list.txt
+
+# Step 2: generate detection results
 python test.py --path nomnaocr-val --model best.pt --output nomnaocr-val-detection
-# evaluate detection results
+# Step 3: evaluate detection results
 python evaluate_detection_yolo.py --pred_dir nomnaocr-val-detection --gt_dir nomnaocr-val-roboflow/labels
